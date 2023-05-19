553a5ab18202c2e80373cb489268baba3f4a14a1
laljidp <yuvalprajapati572@gmail.com>
2023-05-19 10:33:21 +0530

    rendered notiboy logo for smaller screens.

--- src/components/PrivateHeader/index.tsx
+++ src/components/PrivateHeader/index.tsx
@@ -1,14 +1,14 @@
-import { Box, Button, Icon } from '@chakra-ui/react';
+import { Box, Button, Hide, Icon, Image, Show } from '@chakra-ui/react';
 import { AlgorandIcon } from '../../assets/svgs';
 // import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 import { FiSettings } from 'react-icons/all';
 import WalletDropdown from '../Wallets/WalletDropdown';
 import { useNavigate } from 'react-router-dom';
 import { routes } from '../../config';
+import NotiboyLogo from '../../assets/images/notiboy.png';
 
 export default function PrivateHeader() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <Box p={5}>
@@ -18,9 +18,14 @@ export default function PrivateHeader() {
         alignItems={'center'}
       >
         <Box>
-          <Icon h={30} width={30} fill={'blue.400'}>
-            <AlgorandIcon />
-          </Icon>
+          <Show below="md">
+            <Image src={NotiboyLogo} height={50} width={50} />
+          </Show>
+          <Hide below="md">
+            <Icon h={30} width={30} fill={'blue.400'}>
+              <AlgorandIcon />
+            </Icon>
+          </Hide>
         </Box>
         <Box display={'flex'} justifyItems={'ce'}>
           {/* <ColorModeSwitcher /> */}
