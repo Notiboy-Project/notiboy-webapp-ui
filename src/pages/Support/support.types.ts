23a9a3e519cb8853c1f82dbca0484967ccef5487
Vidhyanandcs <vithushaji@gmail.com>
2023-05-30 20:07:37 +0530

    support questions update based on feedback

--- src/pages/Support/support.types.ts
+++ src/pages/Support/support.types.ts
@@ -8,23 +8,23 @@ export const FAQS_DATA: SupportDto[] = [
     id: 2,
     question: 'How much does it cost to send notifications ?',
     answer:
-      'Sending notifications is free of cost.But in free tire there will be restrictions in the number of notifications that can be sent.'
+      'Sending notifications is free of cost. But in free tire there will be restrictions in the number of notifications that can be sent.'
   },
   {
     id: 3,
     question: 'How can I start receiving notifications ?',
-    answer: `You can start getting notifications by opting-in to a channel. When you 
-      opt-out of a channel you will stop getting notifications.`
+    answer: `You can start receiving notifications by opting-in to a channel. When you 
+      opt-out of a channel you will stop receiving notifications. It is free to opt-in and opt-out of a channel.`
   },
   {
     id: 4,
     question: ' Can I get notifications via email and discord ?',
-    answer: ` Yes, you can start getting notifications via email and discord by verifying via settings page.`
+    answer: ` Yes, you can start getting notifications via discord by verifying via settings page. Email notifications coming soon.`
   },
   {
     id: 5,
     question: ' Are there any premium plans ?',
-    answer: `Yes, premium plans cost USD 9 and USD 29 per month which will give you access to extra features.`
+    answer: `Yes, premium plans cost USD 9 and USD 29 per month which will give access to extra features for channel creators. We are working on a subscription model which will be released soon.`
   }
 ];
 
