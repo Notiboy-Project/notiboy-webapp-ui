bbc099e2ab014d8583c870e6b6410d4e5f65cabd
laljidp <yuvalprajapati572@gmail.com>
2023-06-19 18:59:18 +0530

    feat: implemented service worker for notification.

--- src/components/PushNotification/index.tsx
+++ src/components/PushNotification/index.tsx
@@ -2,61 +2,110 @@ import { useContext, useEffect } from 'react';
 import { UserContext } from '../../Context/userContext';
 import { envs } from '../../config';
 import { getTokenFromStorage } from '../../services/storage.service';
-import { REFRESH_NOTIFICATIONS, publish } from '../../services/events.service';
+// import { REFRESH_NOTIFICATIONS, publish } from '../../services/events.service';
 
 export default function PushNotificationService() {
   const { user } = useContext(UserContext);
 
-  const handleWebsocketConnection = () => {
-    const token = getTokenFromStorage();
+  const check = () => {
+    if (!('serviceWorker' in navigator)) {
+      console.error('No Service Worker support!');
+    }
+    if (!('PushManager' in window)) {
+      console.error('No Push API Support!');
+    }
+  };
 
-    if (!token) {
-      return null;
+  const requestNotificationPermission = async () => {
+    if (Notification.permission === 'granted') {
+      return;
     }
 
-    const socket = new WebSocket(
-      `${envs.websocketUrl}?chain=${user?.chain}&address=${user?.address}&token=${token}`
-    );
+    const permission = await window.Notification.requestPermission();
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+    if (permission !== 'granted') {
+      console.error('Permission not granted for Notification');
+      //TODO: can show toast message as well here
+    }
+  };
 
-    socket.onmessage = onMessage;
-    socket.onerror = onError;
-    socket.onclose = onClose;
-
-    socket.addEventListener('open', function (event) {
-      console.log('WebSocket connection established.');
-    });
-
-    if ('Notification' in window && Notification.permission !== 'granted') {
-      Notification.requestPermission();
+  const registerNotificationServiceWorker = async () => {
+    check();    
+    await requestNotificationPermission();
+    const config = {
+      chain: user?.chain,
+      address: user?.address,
+      accessToken: getTokenFromStorage(),
+      socketUrl: envs.websocketUrl
     }
 
-    return socket;
+  
+    navigator.serviceWorker
+      .register(`service-nf.js?data=${JSON.stringify(config)}`)
+      .then((serviceWorker) => {
+        console.log('Service worker registered', serviceWorker);
+      })
+      .catch((error) => {
+        console.log('Service worker failed to register', error);
+      });
   };
 
-  const onMessage = (event: any) => {
-    const data = JSON.parse(event.data);
-    publish(REFRESH_NOTIFICATIONS, {});
-    console.log('Received message: ' + data);
-    const nf = new Notification('Notiboy', { body: data?.message || '' });
-    console.log({ nf });
-  };
+  // const handleWebsocketConnection = () => {
+  //   const token = getTokenFromStorage();
 
-  const onError = (event: any) => {
-    console.log('WebSocket connection failed.==>>', event);
-  };
+  //   if (!token) {
+  //     return null;
+  //   }
 
-  const onClose = (event: any) => {
-    console.log('WebSocket connection closed. ==>', event);
-    setTimeout(handleWebsocketConnection, 3000);
-  };
+  //   const socket = new WebSocket(
+  //     `${envs.websocketUrl}?chain=${user?.chain}&address=${user?.address}&token=${token}`
+  //   );
+
+  //   socket.onmessage = onMessage;
+  //   socket.onerror = onError;
+  //   socket.onclose = onClose;
+
+  //   socket.addEventListener('open', function (event) {
+  //     console.log('WebSocket connection established.');
+  //   });
+
+  //   if ('Notification' in window && Notification.permission !== 'granted') {
+  //     Notification.requestPermission();
+  //   }
+
+  //   return socket;
+  // };
+
+  // const onMessage = (event: any) => {
+  //   const data = JSON.parse(event.data);
+  //   publish(REFRESH_NOTIFICATIONS, {});
+  //   console.log('Received message: ' + data);
+  //   const nf = new Notification('Notiboy', { body: data?.message || '' });
+  //   console.log({ nf });
+  // };
+
+  // const onError = (event: any) => {
+  //   console.log('WebSocket connection failed.==>>', event);
+  // };
+
+  // const onClose = (event: any) => {
+  //   console.log('WebSocket connection closed. ==>', event);
+  //   setTimeout(handleWebsocketConnection, 3000);
+  // };
 
   useEffect(() => {
-    const socket = handleWebsocketConnection();
+    // const socket = handleWebsocketConnection();
+    registerNotificationServiceWorker();
+
+    const socket = null;
 
     return () => {
       if (socket) {
         console.log('socket closing...', socket);
-        socket?.close();
+        // socket?.close();
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
