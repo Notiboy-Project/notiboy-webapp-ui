b45a47fc2e9302b0db3d5c7d74e2d26f6bfa0bcf
laljidp <yuvalprajapati572@gmail.com>
2023-06-20 13:48:07 +0530

    fix issue with service worker notifications.

--- public/service-nf.js
+++ public/service-nf.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
 // eslint-disable-next-line no-restricted-globals
 const data = new URL(location).searchParams.get('data');
 
 const config = JSON.parse(data) || null;
 
-// eslint-disable-next-line no-restricted-globals
 self.addEventListener('activate', async () => {
   // This will be called only once when the service worker is activated.
   console.log('service worker activate');
@@ -36,8 +37,7 @@ function onMessage(event) {
   const data = JSON.parse(event.data);
   // publish(REFRESH_NOTIFICATIONS, {});
   console.log('Received message: ' + data);
-  const nf = new Notification('Notiboy', { body: data?.message || '' });
-  console.log({ nf });
+  dispalyNotification(data);
 }
 
 function onError(event) {
@@ -45,5 +45,36 @@ function onError(event) {
 }
 
 function onClose(event) {
-  console.log('WebSocket connection closed. ==>', event);  
+  console.log('WebSocket connection closed. ==>', event);
 }
+
+function dispalyNotification(data) {
+  // TODO: show notification using registration
+  self.registration.showNotification('Notiboy', {
+    body: data?.message,
+    icon: 'notiboy.png',
+    data: { url: `${self.location.origin}/notifications` }
+  });
+}
+
+self.addEventListener('notificationclick', (e) => {
+  // Close the notification popout
+  e.notification.close();
+  console.log('Notification clicked')
+  // Get all the Window clients
+  e.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientsArr) => {
+      // If a Window tab matching the targeted URL already exists, focus that;
+      const hadWindowToFocus = clientsArr.some((windowClient) =>
+        windowClient.url === e.notification.data.url
+          ? (windowClient.focus(), true)
+          : false
+      );
+      // Otherwise, open a new tab to the applicable URL and focus it.
+      if (!hadWindowToFocus)
+        clients
+          .openWindow(e.notification.data.url)
+          .then((windowClient) => (windowClient ? windowClient.focus() : null));
+    })
+  );
+});
